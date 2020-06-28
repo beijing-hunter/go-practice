f6c097217d3f7d2e1a158482f17c079ce95a6afe
Hunter <aasssitya@163.com>
2020-06-28 09:05:48 +0800

    add

--- src/cronexprdemo/cron_usage/demo01/main.go
+++ src/cronexprdemo/cron_usage/demo01/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "github.com/gorhill/cronexpr"
+import (
+	"fmt"
+	"github.com/gorhill/cronexpr"
+	"time"
+)
 
 func main()  {
-	
+
+	var (
+		expr *cronexpr.Expression
+		err error
+		now time.Time
+	)
+
+	//每1分钟执行
+	if expr,err=cronexpr.Parse("* * * * *");err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	//每5分钟执行1次
+	if expr,err=cronexpr.Parse("*/5 * * * *");err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	//每1毫秒执行1次
+	if expr,err=cronexpr.Parse("* * * * * * *");err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	now=time.Now()
+	//下次调度时间
+	nextTime:=expr.Next(now)
+	fmt.Println(now,nextTime)
+
+	time.AfterFunc(nextTime.Sub(now), func() {
+		fmt.Println("被调度了：",nextTime)
+	})
+
+	time.Sleep(5*time.Second)
+
 }
---
+++ src/cronexprdemo/cron_usage/demo02/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorhill/cronexpr"
+	"time"
+)
+
+type CronJob struct {
+	expr *cronexpr.Expression
+	nextTime time.Time
+	exec execFuc
+}
+
+type execFuc func()
+
+func main()  {
+
+	var(
+		now time.Time
+		expr *cronexpr.Expression
+		scheduleTable map[string]*CronJob
+		cronJob *CronJob
+	)
+
+	now=time.Now()
+	scheduleTable=make(map[string]*CronJob)
+
+	expr=cronexpr.MustParse("*/5 * * * * * *")
+	cronJob=&CronJob{
+		expr:expr,
+		nextTime:expr.Next(now),
+		exec: func() {
+			fmt.Print("执行job1")
+		},
+	}
+
+	//注册任务
+	scheduleTable["job1"]=cronJob
+
+	expr=cronexpr.MustParse("*/6 * * * * * *")
+	cronJob=&CronJob{
+		expr:expr,
+		nextTime:expr.Next(now),
+		exec: func() {
+			fmt.Println("执行job2")
+		},
+	}
+
+	scheduleTable["job2"]=cronJob
+
+	go func() {
+		for {
+
+			var (
+				cronJob *CronJob
+				now time.Time
+			)
+			now=time.Now()
+			for _,cronJob=range scheduleTable{
+
+				if cronJob.nextTime.Before(now)||cronJob.nextTime.Equal(now){
+					go cronJob.exec()
+					cronJob.nextTime=cronJob.expr.Next(now)
+				}
+			}
+
+			select {
+			case <-time.NewTimer(100*time.Millisecond).C://将在100毫秒可读，返回
+				
+			}
+		}
+	}()
+
+	time.Sleep(60*time.Second)
+}
